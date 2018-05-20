93c0913084db6416aa63ea54b7bc1aa7c8ce452b
Prof-MohamedAtef <proofesser@gmail.com>
2018-05-21 01:02:32 +0200

    S03.01-Solution-RecyclerViewLayout_BasedVideo15Lesson4_Instructions_Diff

--- app/src/main/java/prof/mo/ed/sunshine2018/GreenAdapter.java
+++ app/src/main/java/prof/mo/ed/sunshine2018/GreenAdapter.java
@@ -1,7 +1,9 @@
 package prof.mo.ed.sunshine2018;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -15,20 +17,29 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
     private static final String TAG = GreenAdapter.class.getSimpleName();
     private int mNumberItems;
 
+    public GreenAdapter(int NumberOfItems){
+        mNumberItems=NumberOfItems;
+    }
     @NonNull
     @Override
-    public NumberViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public NumberViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        Context context=viewGroup.getContext();
+        int LayoutInflaterListItem=R.layout.number_list_item;
+        LayoutInflater inflater=LayoutInflater.from(context);
+        boolean showAttachToParentimmediatelly=false;
+        View view= inflater.inflate(LayoutInflaterListItem,viewGroup, showAttachToParentimmediatelly);
+        NumberViewHolder viewHolder=new NumberViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull NumberViewHolder holder, int position) {
-
+        holder.bind(position);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mNumberItems;
     }
 
     public class NumberViewHolder extends RecyclerView.ViewHolder{
@@ -43,5 +54,4 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
             listItemNumberView.setText(String.valueOf(listIndex));
         }
     }
-
 }
