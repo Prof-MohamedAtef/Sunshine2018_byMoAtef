d7450429be93daa51586a94908927b980987b87d
Prof-MohamedAtef <proofesser@gmail.com>
2018-05-20 17:18:21 +0200

    T02.02-Solution-AddMenu

--- app/src/main/java/prof/mo/ed/sunshine2018/MainActivity.java
+++ app/src/main/java/prof/mo/ed/sunshine2018/MainActivity.java
@@ -17,8 +17,11 @@ package prof.mo.ed.sunshine2018;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,6 +53,29 @@ public class MainActivity extends AppCompatActivity {
 
 
     // TODO (8) Override onCreateOptionsMenu
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    String textToShow = "Search clicked";
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId==R.id.action_search){
+            Toast.makeText(getApplicationContext(), textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+
+    }
+
     // TODO (9) Within onCreateOptionsMenu, use getMenuInflater().inflate to inflate the menu
     // TODO (10) Return true to display your menu
 
@@ -58,4 +84,5 @@ public class MainActivity extends AppCompatActivity {
     // TODO (13) If the item's ID is R.id.action_search, show a Toast and return true to tell Android that you've handled this menu click
     // TODO (14) Don't forgot to call .show() on your Toast
     // TODO (15) If you do NOT handle the menu click, return super.onOptionsItemSelected to let Android handle the menu click
+
 }
