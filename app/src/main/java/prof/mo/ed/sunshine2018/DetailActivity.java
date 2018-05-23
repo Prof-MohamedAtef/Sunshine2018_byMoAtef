f5b5500dcc086bd20ebcca52398149db268f32d6
Prof-MohamedAtef <proofesser@gmail.com>
2018-05-23 21:14:24 +0200

    S04.01-Solution-LaunchNewActivity

---
+++ app/src/main/java/prof/mo/ed/sunshine2018/DetailActivity.java
@@ -0,0 +1,16 @@
+package prof.mo.ed.sunshine2018;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+// COMPLETED (1) Create a new Activity called DetailActivity using Android Studio's wizard
+public class DetailActivity extends AppCompatActivity {
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
--- app/src/main/java/prof/mo/ed/sunshine2018/MainActivity.java
+++ app/src/main/java/prof/mo/ed/sunshine2018/MainActivity.java
@@ -16,6 +16,7 @@
 package prof.mo.ed.sunshine2018;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -27,7 +28,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 import java.net.URL;
@@ -117,11 +117,10 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapter.F
     @Override
     public void onClick(String weatherForDay) {
         Context context = this;
-        // TODO (1) Create a new Activity called DetailActivity using Android Studio's wizard
-        // TODO (2) Change the root layout of activity_detail.xml to a FrameLayout and remove unnecessary xml attributes
-        // TODO (3) Remove the Toast and launch the DetailActivity using an explicit Intent
-        Toast.makeText(context, weatherForDay, Toast.LENGTH_SHORT)
-                .show();
+        // COMPLETED (3) Remove the Toast and launch the DetailActivity using an explicit Intent
+        Class destinationClass = DetailActivity.class;
+        Intent intentToStartDetailActivity = new Intent(context, destinationClass);
+        startActivity(intentToStartDetailActivity);
     }
 
     /**
