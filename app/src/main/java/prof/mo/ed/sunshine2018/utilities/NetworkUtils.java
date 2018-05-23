2497fa96907b73f3e0f2c89f69d753c5323061f3
Prof-MohamedAtef <proofesser@gmail.com>
2018-05-24 00:48:59 +0200

    T05b.03-Exercise-PolishAsyncTask

--- app/src/main/java/prof/mo/ed/sunshine2018/utilities/NetworkUtils.java
+++ app/src/main/java/prof/mo/ed/sunshine2018/utilities/NetworkUtils.java
@@ -24,6 +24,9 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
+/**
+ * These utilities will be used to communicate with the network.
+ */
 public class NetworkUtils {
 
     final static String GITHUB_BASE_URL =
@@ -31,9 +34,19 @@ public class NetworkUtils {
 
     final static String PARAM_QUERY = "q";
 
+    /*
+     * The sort field. One of stars, forks, or updated.
+     * Default: results are sorted by best match if no field is specified.
+     */
     final static String PARAM_SORT = "sort";
     final static String sortBy = "stars";
 
+    /**
+     * Builds the URL used to query Github.
+     *
+     * @param githubSearchQuery The keyword that will be queried for.
+     * @return The URL to use to query the weather server.
+     */
     public static URL buildUrl(String githubSearchQuery) {
         Uri builtUri = Uri.parse(GITHUB_BASE_URL).buildUpon()
                 .appendQueryParameter(PARAM_QUERY, githubSearchQuery)
@@ -50,6 +63,13 @@ public class NetworkUtils {
         return url;
     }
 
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
     public static String getResponseFromHttpUrl(URL url) throws IOException {
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
         try {
