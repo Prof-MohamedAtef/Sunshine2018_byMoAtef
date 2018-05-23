03ae312253807e9dd5948d3ec99adf163bb4c01a
Prof-MohamedAtef <proofesser@gmail.com>
2018-05-23 21:26:31 +0200

    T04a.02-Solution-StartNewActivity

--- app/src/main/java/prof/mo/ed/sunshine2018/ChildActivity.java
+++ app/src/main/java/prof/mo/ed/sunshine2018/ChildActivity.java
@@ -1,17 +1,19 @@
 package prof.mo.ed.sunshine2018;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class ChildActivity extends AppCompatActivity {
 
-
+    /* Field to store our TextView */
     private TextView mDisplayText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
-        mDisplayText=(TextView)findViewById(R.id.tv_display);
+
+        /* Typical usage of findViewById... */
+        mDisplayText = (TextView) findViewById(R.id.tv_display);
     }
 }
