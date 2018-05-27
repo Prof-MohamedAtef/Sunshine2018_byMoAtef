a4fb30ca0234a478673bbbda06ffb79a954599dc
Prof-MohamedAtef <proofesser@gmail.com>
2018-05-28 01:17:33 +0200

    T06.02-Exercise-MakeAPreferenceFragment

---
+++ app/src/main/java/prof/mo/ed/sunshine2018/SettingsFragment.java
@@ -0,0 +1,20 @@
+package prof.mo.ed.sunshine2018;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import prof.mo.ed.sunshine2018.R;
+
+/**
+ * Created by Prof-Mohamed Atef on 5/28/2018.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_visualizer);
+    }
+}
