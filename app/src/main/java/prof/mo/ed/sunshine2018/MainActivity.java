a1417db55a3c19d04ef768c40097c8b83fd04fa1
Prof-MohamedAtef <proofesser@gmail.com>
2018-05-29 18:16:03 +0200

    T08.02-Exercise-AddAsyncTaskToRetrieveCursor

--- app/src/main/java/prof/mo/ed/sunshine2018/MainActivity.java
+++ app/src/main/java/prof/mo/ed/sunshine2018/MainActivity.java
@@ -16,6 +16,8 @@
 
 package prof.mo.ed.sunshine2018;
 
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -30,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     // The current state of the app
     private int mCurrentState;
 
-
+    // TODO (3) Create an instance variable storing a Cursor called mData
     private Button mButton;
 
     // This state is when the word definition is hidden and clicking the button will therefore
@@ -49,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
 
         // Get the views
         mButton = (Button) findViewById(R.id.button_next);
+
+        // TODO (5) Create and execute your AsyncTask here
     }
 
     /**
@@ -87,5 +91,11 @@ public class MainActivity extends AppCompatActivity {
         mCurrentState = STATE_SHOWN;
 
     }
+
+    // TODO (1) Create AsyncTask with the following generic types <Void, Void, Cursor>
+
+    // TODO (2) In the doInBackground method, write the code to access the DroidTermsExample
+    // provider and return the Cursor object
+    // TODO (4) In the onPostExecute method, store the Cursor object in mData
 
 }
