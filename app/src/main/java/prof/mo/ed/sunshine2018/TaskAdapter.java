3ac00b62b5f0b89b942502900bd14c992a0bdd9a
Prof-MohamedAtef <proofesser@gmail.com>
2018-06-01 11:32:18 +0200

    T09b.02-Exercise-SaveTaskInDatabaseFromAddTaskActivity

--- app/src/main/java/prof/mo/ed/sunshine2018/TaskAdapter.java
+++ app/src/main/java/prof/mo/ed/sunshine2018/TaskAdapter.java
@@ -24,7 +24,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Locale;
